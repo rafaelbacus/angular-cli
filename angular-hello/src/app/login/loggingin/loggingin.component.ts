7486cb1c0d3d2e315347e13559b70cef87ebaa93
Rafael Bacus <rafael.bacus@gmail.com>
2020-08-15 22:51:22 -0400

    generating ngmodules

---
+++ angular-hello/src/app/login/loggingin/loggingin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loggingin',
+  templateUrl: './loggingin.component.html',
+  styleUrls: ['./loggingin.component.scss']
+})
+export class LogginginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
